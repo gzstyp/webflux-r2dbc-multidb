334de8113b0cf7918999294e493d5362a6f294aa
引路者 <444141300@qq.com>
2021-05-23 11:18:46 +0800

    升级版本号,仅支持MySQL5.7.xx

--- src/main/java/com/fwtai/webflux/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.fwtai.webflux.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
-@Configuration
-@EnableWebFlux
-public class AppConfig{}
